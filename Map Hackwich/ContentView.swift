6567d2acc2d5cc692b475d57bdb461bde96406fb
Zane <zmatarieh@bsd220.org>
2025-02-03 09:41:37 -0600

    setup initial map

--- Map Hackwich/ContentView.swift
+++ Map Hackwich/ContentView.swift
@@ -6,16 +6,27 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @State private var startPosition = MapCameraPosition.region(
+
+            MKCoordinateRegion(center: CLLocationCoordinate2D(
+
+                latitude: 42.15559,
+
+                longitude: -88.14692),
+
+            span: MKCoordinateSpan(
+
+                latitudeDelta: 0.05,
+
+                longitudeDelta: 0.05)
+
+            )
+        )
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Map(position: $startPosition)
     }
 }
 
