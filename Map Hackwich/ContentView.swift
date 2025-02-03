b2be2a516002e7adf5ed16ef15507a5b5adc4aa4
Zane <zmatarieh@bsd220.org>
2025-02-03 10:00:36 -0600

    addded user location

--- Map Hackwich/ContentView.swift
+++ Map Hackwich/ContentView.swift
@@ -9,24 +9,17 @@ import SwiftUI
 import MapKit
 
 struct ContentView: View {
-    @State private var startPosition = MapCameraPosition.region(
+    @StateObject var locationManager = LocationManager()
+    @State private var startPosition = MapCameraPosition.userLocation(fallback: .automatic)
+    
 
-            MKCoordinateRegion(center: CLLocationCoordinate2D(
-
-                latitude: 42.15559,
-
-                longitude: -88.14692),
-
-            span: MKCoordinateSpan(
-
-                latitudeDelta: 0.05,
-
-                longitudeDelta: 0.05)
-
-            )
-        )
     var body: some View {
-        Map(position: $startPosition)
+        
+        Map(position: $startPosition) {
+
+                    UserAnnotation()
+
+                }
     }
 }
 
---
+++ Map Hackwich/LocationManager.swift
@@ -0,0 +1,26 @@
+//
+//  LocationManager.swift
+//  Map Hackwich
+//
+//  Created by Zane Matarieh on 2/3/25.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
+
+    var locationManager = CLLocationManager()
+    
+    override init() {
+
+        super.init()
+
+        locationManager.delegate = self
+
+        locationManager.requestWhenInUseAuthorization()
+
+        locationManager.startUpdatingLocation()
+
+    }
+}
